8e8d823ed90b6a1977b54bc00592f4f2d140532d
ThomasMaugeri <maugerithomas73@gamil.com>
2023-11-13 16:16:17 +0100

    dja

--- thomas/src/components/pag3.vue
+++ thomas/src/components/pag3.vue
@@ -26,23 +26,23 @@ IS GRACE
             <img src="https://grace.energy/wp-content/uploads/2023/06/grace-box.jpg" alt="">
          </div> 
       </div>
-      <div class="card2">
 
-         <div class="leftcard1">
-            <div class="numbercard">
-            <span>1</span>   
-            </div>
-            <div class="textcard">
-               <h2>aminoacidi</h2>
-               <span class="higher">100% higher</span>
-               <p>On average, GRACE yolks' amino acidslevel is 100% higher than other yolks</p>
-               <span class="buy">buy</span>
+      <div class="card2">
+         <div class="leftcard2">
+            <img src="https://grace.energy/wp-content/uploads/2023/06/grace-box.jpg" alt="">
             </div>
 
          </div>
-         <div class="rightcard1">
-            <img src="https://grace.energy/wp-content/uploads/2023/06/grace-box.jpg" alt="">
-         </div> 
+         <div class="rightcard2">
+           <div class="numbercard2">
+            <span>2</span>   
+            </div>
+            <div class="textcard2">
+               <h2>antioxidant</h2>
+               <span class="awarded">AWARDED BY THE EUROPEAN ANTIOXIDANT INSTITUTE</span>
+               <p>The PAOT Score expresses the antioxidant power of a product, measuring its ability to neutralize free radicals. In April 2019, the European Antioxidant Institute (IEA) awarded GRACE a PAOT score of 65.4/g, demonstrating its very high antioxidant power.</p>
+               <span class="buy2">buy</span>
+            </div>
       </div>
          
    </div>
@@ -53,13 +53,13 @@ IS GRACE
 
 .bgpag3{
    background-color: black;
-
+   }
    .pag3{
    width:90%;
    margin: auto;
    padding-top:50px ;
    color:white;
-
+   }   
    .card1{
       width: 100%;
       display:flex;
@@ -105,67 +105,46 @@ IS GRACE
                color:black;
                padding: 5px 3px 0 3px;
             }
-         }   
+         }  
+         .textcard2{
+
+         } 
       }
       .rightcard1{
-
+         width: 40%;
       }
    }  
    .card2{
-      display:flex;
+      display: flex;
       flex-direction: row-reverse;
       width: 100%;
-      display:flex;
-      .leftcard1{
+      .leftcard2{
          width: 40%;
-         display: flex;
-         .numbercard{
-            width: 10%;
+      }
+         .rightcard2{
+            width: 60%;
             display: flex;
-            justify-content: center;
-            padding-top: 200px;
-            font-size: 30px;
-          span{
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            width: 50px;
-            height: 50px;
-            border: 2px solid #fbed3f;
-            border-radius:50%;
-            color: #fbed3f;
-          }
          }
-         .textcard{
-            width:90%;
-            h2{
+            .numbercard2{
+               width: 10%;
+               display: flex;
+               justify-content: center;
                padding-top: 200px;
                font-size: 30px;
-               padding-bottom: 20px;
-            }  
-            .higher{
-               opacity: .6;
-               font-size: 20px;
-            }
-            p{
-               padding-top:20px;
-               font-size: 20px;
-               padding-bottom: 20px;
-            }
-            .buy{
-               background-color: #fbed3f;
-               font-size: 20px;
-               color:black;
-               padding: 5px 3px 0 3px;
-            }
-         }   
-      }
-      .rightcard1{
-         width: 60%;
-      }
+
+                   span{
+                     display: flex;
+                     align-items: center;
+                     justify-content: center;
+                     width: 50px;
+                     height: 50px;
+                     border: 2px solid #fbed3f;
+                     border-radius:50%;
+                     color: #fbed3f;
+              }
+              
+       } 
+
+
    }
-}
-}
-
-
-</style>
+    </style>
